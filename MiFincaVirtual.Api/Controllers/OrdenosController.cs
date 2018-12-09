20e0f30163372e01eaece02638810fd1249332f1
andresftm <40926960+andresftm@users.noreply.github.com>
2018-12-08 23:02:22 -0500

    Update OrdenosController.cs

--- MiFincaVirtual.Api/Controllers/OrdenosController.cs
+++ MiFincaVirtual.Api/Controllers/OrdenosController.cs
@@ -1,124 +1,133 @@
-﻿using System;
+using MiFincaVirtual.Api.Models;
+using MiFincaVirtual.Api.Tools;
+using MiFincaVirtual.Common.Models;
+using MiFincaVirtual.Domain.Models;
+using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
-using MiFincaVirtual.Common.Models;
-using MiFincaVirtual.Domain.Models;
 
 namespace MiFincaVirtual.Api.Controllers
 {
-    [Authorize]
-    public class OrdenosController : ApiController
+  [Authorize]
+  public class OrdenosController : ApiController
+  {
+    private DataContext db;
+
+    public IEnumerable<Ordenos> GetOrdenos()
     {
-        private DataContext db = new DataContext();
-
-        // GET: api/Ordenos
-        public IQueryable<Ordenos> GetOrdenos()
-        {
-            return db.Ordenos.OrderByDescending(o => o.OrdenoId).Take(15);
-        }
-
-        // GET: api/Ordenos/5
-        [ResponseType(typeof(Ordenos))]
-        public async Task<IHttpActionResult> GetOrdenos(int id)
-        {
-            Ordenos ordenos = await db.Ordenos.FindAsync(id);
-            if (ordenos == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(ordenos);
-        }
-
-        // PUT: api/Ordenos/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutOrdenos(int id, Ordenos ordenos)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != ordenos.OrdenoId)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(ordenos).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!OrdenosExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Ordenos
-        [ResponseType(typeof(Ordenos))]
-        public async Task<IHttpActionResult> PostOrdenos(Ordenos ordenos)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            ordenos.FechaOrdeno = ordenos.FechaOrdeno.ToUniversalTime();
-            ordenos.PesoOrdeno = 10;
-
-            db.Ordenos.Add(ordenos);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = ordenos.OrdenoId }, ordenos);
-        }
-
-        // DELETE: api/Ordenos/5
-        [ResponseType(typeof(Ordenos))]
-        public async Task<IHttpActionResult> DeleteOrdenos(int id)
-        {
-            Ordenos ordenos = await db.Ordenos.FindAsync(id);
-            if (ordenos == null)
-            {
-                return NotFound();
-            }
-
-            db.Ordenos.Remove(ordenos);
-            await db.SaveChangesAsync();
-
-            return Ok(ordenos);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool OrdenosExists(int id)
-        {
-            return db.Ordenos.Count(e => e.OrdenoId == id) > 0;
-        }
+      List<ConsultaOrdeno> source = new List<ConsultaOrdeno>();
+      using (LocalDataContext localDataContext = new LocalDataContext())
+        source = ((IEnumerable<ConsultaOrdeno>) ((DbContext) localDataContext).get_Database().SqlQuery<ConsultaOrdeno>(Sp.uspOrdenosConsultar, (object[]) Array.Empty<object>())).ToList<ConsultaOrdeno>();
+      return source.Select<ConsultaOrdeno, Ordenos>((Func<ConsultaOrdeno, Ordenos>) (o => new Ordenos()
+      {
+        Animal = o.Animal,
+        AnimalId = new int?(o.AnimalId),
+        FechaOrdeno = o.FechaOrdeno,
+        LitrosOrdeno = o.LitrosOrdeno,
+        NumeroOrdeno = o.NumeroOrdeno,
+        PesoOrdeno = o.PesoOrdeno,
+        OrdenoId = o.OrdenoId,
+        GramosCuidoOrdeno = o.GramosCuidoOrdeno
+      })).Take<Ordenos>(20);
     }
+
+    [ResponseType(typeof (Ordenos))]
+    public IEnumerable<Ordenos> GetOrdenos(int id)
+    {
+      List<ConsultaOrdeno> source = new List<ConsultaOrdeno>();
+      if (id == 0)
+      {
+        using (LocalDataContext localDataContext = new LocalDataContext())
+          source = ((IEnumerable<ConsultaOrdeno>) ((DbContext) localDataContext).get_Database().SqlQuery<ConsultaOrdeno>(Sp.uspBovinosGestantesConsultar, (object[]) Array.Empty<object>())).ToList<ConsultaOrdeno>();
+        return source.Select<ConsultaOrdeno, Ordenos>((Func<ConsultaOrdeno, Ordenos>) (o => new Ordenos()
+        {
+          Animal = o.Animal,
+          AnimalId = new int?(o.AnimalId)
+        })).Take<Ordenos>(20);
+      }
+      using (LocalDataContext localDataContext = new LocalDataContext())
+        source = ((IEnumerable<ConsultaOrdeno>) ((DbContext) localDataContext).get_Database().SqlQuery<ConsultaOrdeno>(Sp.uspBovinosGestantesConsultar, (object[]) Array.Empty<object>())).ToList<ConsultaOrdeno>();
+      return source.Select<ConsultaOrdeno, Ordenos>((Func<ConsultaOrdeno, Ordenos>) (o => new Ordenos()
+      {
+        Animal = o.Animal,
+        AnimalId = new int?(o.AnimalId)
+      })).Take<Ordenos>(20);
+    }
+
+    [ResponseType(typeof (void))]
+    public async Task<IHttpActionResult> PutOrdenos(int id, Ordenos ordenos)
+    {
+      OrdenosController ordenosController = this;
+      if (!ordenosController.get_ModelState().get_IsValid())
+        return (IHttpActionResult) ordenosController.BadRequest(ordenosController.get_ModelState());
+      if (id != ordenos.OrdenoId)
+        return (IHttpActionResult) ordenosController.BadRequest();
+      ((DbEntityEntry<Ordenos>) ((DbContext) ordenosController.db).Entry<Ordenos>((M0) ordenos)).set_State((EntityState) 16);
+      try
+      {
+        int num = await ((DbContext) ordenosController.db).SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException ex)
+      {
+        if (!ordenosController.OrdenosExists(id))
+          return (IHttpActionResult) ordenosController.NotFound();
+        throw;
+      }
+      return (IHttpActionResult) ordenosController.StatusCode(HttpStatusCode.NoContent);
+    }
+
+    [ResponseType(typeof (Ordenos))]
+    public async Task<IHttpActionResult> PostOrdenos(Ordenos ordenos)
+    {
+      OrdenosController ordenosController = this;
+      if (!ordenosController.get_ModelState().get_IsValid())
+        return (IHttpActionResult) ordenosController.BadRequest(ordenosController.get_ModelState());
+      ordenos.FechaOrdeno = ordenos.FechaOrdeno.ToUniversalTime();
+      ordenosController.db.get_Ordenos().Add(ordenos);
+      int num = await ((DbContext) ordenosController.db).SaveChangesAsync();
+      return (IHttpActionResult) ordenosController.CreatedAtRoute<Ordenos>("DefaultApi", (object) new
+      {
+        id = ordenos.OrdenoId
+      }, (M0) ordenos);
+    }
+
+    [ResponseType(typeof (Ordenos))]
+    public async Task<IHttpActionResult> DeleteOrdenos(int id)
+    {
+      OrdenosController ordenosController = this;
+      Ordenos ordenos = await ordenosController.db.get_Ordenos().FindAsync(new object[1]
+      {
+        (object) id
+      });
+      if (ordenos == null)
+        return (IHttpActionResult) ordenosController.NotFound();
+      ordenosController.db.get_Ordenos().Remove(ordenos);
+      int num = await ((DbContext) ordenosController.db).SaveChangesAsync();
+      return (IHttpActionResult) ordenosController.Ok<Ordenos>((M0) ordenos);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+      if (disposing)
+        ((DbContext) this.db).Dispose();
+      base.Dispose(disposing);
+    }
+
+    private bool OrdenosExists(int id)
+    {
+      return ((IQueryable<Ordenos>) this.db.get_Ordenos()).Count<Ordenos>((Expression<Func<Ordenos, bool>>) (e => e.OrdenoId == id)) > 0;
+    }
+
+    public OrdenosController()
+    {
+      base.\u002Ector();
+    }
+  }
 }
